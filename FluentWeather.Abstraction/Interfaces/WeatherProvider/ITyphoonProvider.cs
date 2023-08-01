a36c38ff7b39debdc8a59de426d4e4499c462b96
Betta_Fish <zxbmmmmmmmmm@qq.com>
2023-08-01 11:31:52 +0800

    [feat](abstraction) 添加台风相关API

---
+++ FluentWeather.Abstraction/Interfaces/WeatherProvider/ITyphoonProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentWeather.Abstraction.Interfaces.WeatherProvider
+{
+    internal interface ITyphoonProvider
+    {
+    }
+}
---
+++ FluentWeather.Abstraction/Models/TyphoonBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentWeather.Abstraction.Models
+{
+    internal class TyphonBase
+    {
+    }
+}
