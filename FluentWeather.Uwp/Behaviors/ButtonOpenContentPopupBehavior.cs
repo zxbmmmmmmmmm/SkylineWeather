fcd42427cdaa06f1925043b32eefa504e016fd5c
Betta_Fish <zxbmmmmmmmmm@qq.com>
2023-08-08 16:59:20 +0800

    [feat/WIP] 添加ContentPopup

---
+++ FluentWeather.Uwp/Behaviors/ButtonOpenContentPopupBehavior.cs
@@ -0,0 +1,34 @@
+﻿using FluentWeather.Uwp.Controls.Popups;
+using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace FluentWeather.Uwp.Behaviors;
+
+public class ButtonOpenContentPopupBehavior:Behavior<ButtonBase>
+{
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+        AssociatedObject.Click += ButtonClicked;
+    }
+    
+    public FrameworkElement Content
+    {
+        get => (FrameworkElement)GetValue(ContentProperty);
+        set => SetValue(ContentProperty, value);
+    }
+
+    public static readonly DependencyProperty ContentProperty =
+        DependencyProperty.Register(nameof(Content), typeof(FrameworkElement), typeof(ButtonOpenContentPopupBehavior), new PropertyMetadata(null));
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+        AssociatedObject.Click -= ButtonClicked;
+    }
+    private void ButtonClicked(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+    {
+        new ContentPopup().Show(Content);
+    }
+}
---
+++ FluentWeather.Uwp/Controls/Popups/ContentPopup.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//https://go.microsoft.com/fwlink/?LinkId=234236 上介绍了“用户控件”项模板
+
+namespace FluentWeather.Uwp.Controls.Popups
+{
+    public sealed partial class ContentPopup : UserControl
+    {
+        private Popup _popup;
+        public ContentPopup()
+        {
+            this.InitializeComponent();
+            _popup = new Popup
+            {
+                Child = this,
+                ShouldConstrainToRootBounds = true,
+            };
+        }
+        public void Show(FrameworkElement content,string title = "")
+        {
+            this.Width = Window.Current.Bounds.Width;
+            this.Height = Window.Current.Bounds.Height;
+            Window.Current.SizeChanged += WindowSizeChanged;
+            _popup.IsOpen = true;
+            BackButton.Click += BackButton_Click;
+            if (content is null) return;
+            MainGrid.Children.Add(content);
+            Grid.SetRow(content, 1);
+        }
+
+        private void WindowSizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            this.Width = Window.Current.Bounds.Width;
+            this.Height = Window.Current.Bounds.Height;
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            _popup.IsOpen = false;
+            _popup.Child = null;
+            _popup = null;
+            Window.Current.SizeChanged -= WindowSizeChanged;
+            BackButton.Click -= BackButton_Click;
+            MainGrid.Children.Clear();
+        }
+    }
+}
