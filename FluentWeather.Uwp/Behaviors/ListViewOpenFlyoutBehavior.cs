726108d9a3081c91a6ece028e27598eb3d5956a0
Betta_Fish <zxbmmmmmmmmm@qq.com>
2023-08-10 22:25:24 +0800

    [improve] 优化UI

---
+++ FluentWeather.Uwp/Behaviors/ListViewOpenFlyoutBehavior.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace FluentWeather.Uwp.Behaviors;
+
+public class ListViewOpenFlyoutBehavior : Behavior<ListViewBase>
+{
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+        AssociatedObject.ItemClick += ListItemClicked;
+    }
+
+    private void ListItemClicked(object sender, ItemClickEventArgs e)
+    {
+        var container =AssociatedObject.ContainerFromItem(e.ClickedItem);
+        var item = container as SelectorItem;
+        var content = item?.ContentTemplateRoot as FrameworkElement;
+        if (content is null) return;
+        FlyoutBase.ShowAttachedFlyout(content);
+    }
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+        AssociatedObject.ItemClick -= ListItemClicked;
+    }
+}
