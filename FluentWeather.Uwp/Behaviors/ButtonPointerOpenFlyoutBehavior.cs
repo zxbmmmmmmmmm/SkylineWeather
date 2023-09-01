9ab889f0adb0cedbaa18030718d530e9b7e82823
Betta_Fish <zxbmmmmmmmmm@qq.com>
2023-09-01 12:55:29 +0800

    [feat/WIP] 添加ButtonPointerOpenFlyoutBehavior

---
+++ FluentWeather.Uwp/Behaviors/ButtonPointerOpenFlyoutBehavior.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace FluentWeather.Uwp.Behaviors;
+
+public class ButtonPointerOpenFlyoutBehavior:Behavior<Button>
+{
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+        AssociatedObject.PointerEntered += OnPointerEntered;
+        AssociatedObject.PointerExited += OnPointerExited;
+    }
+
+    private void OnPointerExited(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+    {
+        AssociatedObject?.Flyout?.Hide();
+    }
+
+    private void OnPointerEntered(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+    {
+
+        AssociatedObject?.Flyout?.ShowAt(AssociatedObject);
+    }
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+        AssociatedObject.PointerEntered -= OnPointerEntered;
+        AssociatedObject.PointerExited -= OnPointerExited;
+    }
+}
