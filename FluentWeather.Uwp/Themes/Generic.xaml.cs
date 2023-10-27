0c31521ca157087a15f2ec1cea7477b4ba8e3e1f
Betta_Fish <96322503+zxbmmmmmmmmm@users.noreply.github.com>
2023-10-27 23:48:20 +0800

    [refactor] 将部分UserControl改为DataTemplate

---
+++ FluentWeather.Uwp/Themes/Generic.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Windows.UI.Xaml;
+
+namespace FluentWeather.Uwp.Themes;
+
+public partial class Generic:ResourceDictionary
+{
+    public Generic()
+    {
+        this.InitializeComponent();
+    }
+}
