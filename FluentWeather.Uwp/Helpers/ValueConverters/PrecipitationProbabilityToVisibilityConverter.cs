d82cf874dcd78c825fa95e746e08dcb7f5854e60
Betta_Fish <96322503+zxbmmmmmmmmm@users.noreply.github.com>
2023-10-28 16:21:03 +0800

    [feat] 降水概率显示

---
+++ FluentWeather.Uwp/Helpers/ValueConverters/PrecipitationProbabilityToVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace FluentWeather.Uwp.Helpers.ValueConverters;
+
+public class PrecipitationProbabilityToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is not int precip) return null;
+        return (precip >= 10) ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
