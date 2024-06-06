ceebb1d8bd6997dcecfad8b6d471a78801f71f40
Betta_Fish <zxbmmmmmmmmm@qq.com>
2024-06-06 23:19:42 +0800

    [feat] 历史天气

---
+++ FluentWeather.Abstraction/Interfaces/WeatherProvider/IHistoricalWeatherProvider.cs
@@ -0,0 +1,11 @@
+﻿using FluentWeather.Abstraction.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FluentWeather.Abstraction.Interfaces.WeatherProvider;
+
+public interface IHistoricalWeatherProvider
+{
+    public Task<List<WeatherDailyBase>> GetHistoricalDailyWeather(double lon, double lat, DateTime startTime, DateTime endTime);
+}
