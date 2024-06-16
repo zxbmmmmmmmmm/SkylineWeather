4892757c7708b855982d8bfb02d20f9222208f4f
Betta_Fish <zxbmmmmmmmmm@qq.com>
2024-06-16 11:47:09 +0800

    [refactor] 将历史天气控件化

---
+++ FluentWeather.Uwp/Controls/HistoricalWeatherGauge.xaml.cs
@@ -0,0 +1,50 @@
+﻿using FluentWeather.Abstraction.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//https://go.microsoft.com/fwlink/?LinkId=234236 上介绍了“用户控件”项模板
+
+namespace FluentWeather.Uwp.Controls
+{
+    public sealed partial class HistoricalWeatherGauge : UserControl
+    {
+        public HistoricalWeatherGauge()
+        {
+            this.InitializeComponent();
+        }
+
+        public HistoricalDailyWeatherBase HistoricalDailyWeather
+        {
+            get => (HistoricalDailyWeatherBase)GetValue(HistoricalDailyWeatherProperty);
+            set => SetValue(HistoricalDailyWeatherProperty, value);
+        }
+
+        public static readonly DependencyProperty HistoricalDailyWeatherProperty =
+            DependencyProperty.Register(nameof(HistoricalDailyWeather), typeof(HistoricalDailyWeatherBase), typeof(HistoricalWeatherGauge), new PropertyMetadata(default));
+
+
+
+        public WeatherDailyBase WeatherToday
+        {
+            get => (WeatherDailyBase)GetValue(WeatherTodayProperty);
+            set => SetValue(WeatherTodayProperty, value);
+        }
+
+        public static readonly DependencyProperty WeatherTodayProperty =
+            DependencyProperty.Register(nameof(WeatherToday), typeof(WeatherDailyBase), typeof(HistoricalWeatherGauge), new PropertyMetadata(default));
+
+
+    }
+}
