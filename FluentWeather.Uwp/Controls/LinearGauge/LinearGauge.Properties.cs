da9679ae83ff19171188a642b212feca9fdf8424
Betta_Fish <zxbmmmmmmmmm@qq.com>
2024-09-18 13:02:34 +0800

    [feat] 添加温度指示条

---
+++ FluentWeather.Uwp/Controls/LinearGauge/LinearGauge.Properties.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace FluentWeather.Uwp.Controls;
+
+public partial class LinearGauge
+{
+    private const double DefaultMinimum = 0.0;
+    private const double DefaultMaximum = 10.0;
+    /// <summary>
+    /// Identifies the <see cref="Minimum"/> property.
+    /// </summary>
+    public static readonly DependencyProperty MinimumProperty =
+        DependencyProperty.Register(
+            nameof(Minimum),
+            typeof(double),
+            typeof(LinearGauge),
+            new PropertyMetadata(DefaultMinimum, MinimumChangedCallback));
+
+    /// <summary>
+    /// Identifies the <see cref="Maximum"/> property.
+    /// </summary>
+    public static readonly DependencyProperty MaximumProperty =
+        DependencyProperty.Register(
+            nameof(Maximum),
+            typeof(double),
+            typeof(LinearGauge),
+            new PropertyMetadata(DefaultMaximum, MaximumChangedCallback));
+
+
+    /// <summary>
+    /// Identifies the <see cref="RangeStart"/> property.
+    /// </summary>
+    public static readonly DependencyProperty RangeStartProperty =
+        DependencyProperty.Register(
+            nameof(RangeStart),
+            typeof(double),
+            typeof(LinearGauge),
+            new PropertyMetadata(DefaultMinimum, RangeMinChangedCallback));
+
+    /// <summary>
+    /// Identifies the <see cref="RangeEnd"/> property.
+    /// </summary>
+    public static readonly DependencyProperty RangeEndProperty =
+        DependencyProperty.Register(
+            nameof(RangeEnd),
+            typeof(double),
+            typeof(LinearGauge),
+            new PropertyMetadata(DefaultMaximum, RangeMaxChangedCallback));
+
+
+    public double Minimum
+    {
+        get => (double)GetValue(MinimumProperty);
+        set => SetValue(MinimumProperty, value);
+    }
+
+    /// <summary>
+    /// Gets or sets the absolute maximum value of the range.
+    /// </summary>
+    /// <value>
+    /// The maximum.
+    /// </value>
+    public double Maximum
+    {
+        get => (double)GetValue(MaximumProperty);
+        set => SetValue(MaximumProperty, value);
+    }
+
+    /// <summary>
+    /// Gets or sets the current selected lower limit value of the range, modifiable by the user.
+    /// </summary>
+    /// <value>
+    /// The current lower limit.
+    /// </value>
+    public double RangeStart
+    {
+        get => (double)GetValue(RangeStartProperty);
+        set => SetValue(RangeStartProperty, value);
+    }
+
+    /// <summary>
+    /// Gets or sets the current selected upper limit value of the range, modifiable by the user.
+    /// </summary>
+    /// <value>
+    /// The current upper limit.
+    /// </value>
+    public double RangeEnd
+    {
+        get => (double)GetValue(RangeEndProperty);
+        set => SetValue(RangeEndProperty, value);
+    }
+    private static void MinimumChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var rangeSelector = d as LinearGauge;
+
+        if (rangeSelector == null)
+        {
+            return;
+        }
+
+        rangeSelector.SyncActiveRectangle();
+    }
+
+    private static void MaximumChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var rangeSelector = d as LinearGauge;
+
+        if (rangeSelector == null)
+        {
+            return;
+        }
+
+        rangeSelector.SyncActiveRectangle();
+    }
+
+    private static void RangeMinChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var rangeSelector = d as LinearGauge;
+
+        if (rangeSelector == null)
+        {
+            return;
+        }
+
+        rangeSelector.SyncActiveRectangle();
+    }
+
+    private static void RangeMaxChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var rangeSelector = d as LinearGauge;
+
+        if (rangeSelector == null)
+        {
+            return;
+        }
+
+        rangeSelector.SyncActiveRectangle();
+    }
+}
---
+++ FluentWeather.Uwp/Controls/LinearGauge/LinearGauge.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Metadata;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace FluentWeather.Uwp.Controls;
+
+[TemplateVisualState(Name = NormalState, GroupName = CommonStates)]
+[TemplatePart(Name = "ContainerCanvas", Type = typeof(Canvas))]
+[TemplatePart(Name = "ActiveRectangle", Type = typeof(Border))]
+[TemplateVisualState(Name = DisabledState, GroupName = CommonStates)]
+public sealed partial class LinearGauge : Control
+{
+    internal const string CommonStates = "CommonStates";
+
+    internal const string NormalState = "Normal";
+    internal const string DisabledState = "Disabled";
+
+    private Canvas _containerCanvas;
+    private Border _activeRectangle;
+    private bool _minSet;
+    private bool _maxSet;
+    private bool _valuesAssigned;
+
+    public LinearGauge()
+    {
+        this.DefaultStyleKey = typeof(LinearGauge);
+    }
+    protected override void OnApplyTemplate()
+    {
+
+        if (_containerCanvas != null)
+        {
+            _containerCanvas.SizeChanged -= ContainerCanvas_SizeChanged;
+        }
+
+        IsEnabledChanged -= RangeSelector_IsEnabledChanged;
+
+        // Need to make sure the values can be set in XAML and don't overwrite each other
+        VerifyValues();
+        _valuesAssigned = true;
+
+        _activeRectangle = GetTemplateChild("ActiveRectangle") as Border;
+
+        _containerCanvas = GetTemplateChild("ContainerCanvas") as Canvas;
+
+
+        if (_containerCanvas != null)
+        {
+            _containerCanvas.SizeChanged += ContainerCanvas_SizeChanged;
+        }
+
+        VisualStateManager.GoToState(this, IsEnabled ? NormalState : DisabledState, false);
+
+        IsEnabledChanged += RangeSelector_IsEnabledChanged;
+
+        base.OnApplyTemplate();
+    }
+
+    private void ContainerCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        SyncActiveRectangle();
+    }
+    private void RangeSelector_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+    {
+        VisualStateManager.GoToState(this, IsEnabled ? NormalState : DisabledState, true);
+    }
+
+    private void SyncActiveRectangle()
+    {
+        if (_containerCanvas == null)
+        {
+            return;
+        }
+        if (_activeRectangle == null)
+        {
+            return;
+        }
+        var relativeLeft = ((RangeStart - Minimum) / (Maximum - Minimum)) * ActualWidth ;
+        var relativeRight = ((RangeEnd - Minimum) / (Maximum - Minimum)) * ActualWidth;
+        Canvas.SetLeft(_activeRectangle, relativeLeft);
+        Canvas.SetTop(_activeRectangle, (_containerCanvas.ActualHeight - _activeRectangle!.ActualHeight) / 2);
+        _activeRectangle.Width = Math.Max(0, relativeRight - relativeLeft);
+    }
+    private void VerifyValues()
+    {
+        if (Minimum > Maximum)
+        {
+            Minimum = Maximum;
+            Maximum = Maximum;
+        }
+
+        //if (!_maxSet)
+        //{
+        //    RangeEnd = Maximum;
+        //}
+
+        //if (!_minSet)
+        //{
+        //    RangeStart = Minimum;
+        //}
+
+        if (RangeStart < Minimum)
+        {
+            RangeStart = Minimum;
+        }
+
+        if (RangeEnd < Minimum)
+        {
+            RangeEnd = Minimum;
+        }
+
+        if (RangeStart > Maximum)
+        {
+            RangeStart = Maximum;
+        }
+
+        if (RangeEnd > Maximum)
+        {
+            RangeEnd = Maximum;
+        }
+
+        if (RangeEnd < RangeStart)
+        {
+            RangeStart = RangeEnd;
+        }
+    }
+
+}
