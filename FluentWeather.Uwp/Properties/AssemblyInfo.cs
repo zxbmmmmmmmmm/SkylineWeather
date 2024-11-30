a4e28befe2991269d6356d9c69c8eef3e306a75b
Betta_Fish <zxbmmmmmmmmm@qq.com>
2024-11-30 22:28:41 +0800

    [chores] 更新版权年份至2024

--- FluentWeather.Uwp/Properties/AssemblyInfo.cs
+++ FluentWeather.Uwp/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FluentWeather.Uwp")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
