b1a167646fe387ccc95476a312b3f8324870dbc1
Betta_Fish <zxbmmmmmmmmm@qq.com>
2024-12-01 00:22:45 +0800

    [improve] 优化界面对齐效果

--- FluentWeather.Uwp/Themes/Generic.xaml.cs
+++ FluentWeather.Uwp/Themes/Generic.xaml.cs
@@ -9,9 +9,11 @@ public sealed partial class Generic:ResourceDictionary
     {
         this.InitializeComponent();
         var isEnglish = CultureInfo.CurrentCulture.Name.Contains("en")
-            || CultureInfo.CurrentCulture.Name.Contains("fr");
+                        || CultureInfo.CurrentCulture.Name.Contains("fr") 
+                        || CultureInfo.CurrentCulture.Name.Contains("es");
         this.Add("WeatherDescriptionFontSize", isEnglish ? 16 : 18);
         this.Add("HourlyWeatherDescriptionVisibility", isEnglish ? Visibility.Collapsed : Visibility.Visible);
+        this.Add("DailyWeatherItemVerticalContentAlignment", isEnglish ? VerticalAlignment.Center : VerticalAlignment.Top);
     }
     public static Visibility GetPrecipVisibility(int? precip)
     {
